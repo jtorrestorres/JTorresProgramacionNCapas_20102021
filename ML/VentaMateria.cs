2c5dfcc0d3eb76e31865094b43c45d892ad3c681
Jesús Torres <jtorres@digis01.com>
2021-10-25 17:41:17 -0500

    Venta

---
+++ ML/VentaMateria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    class VentaMateria
+    {
+        public int IdVentaMateria { get; set; }
+        public ML.Venta Venta { get; set; }
+        public ML.Usuario Usuario { get; set; }
+    }
+}
---
+++ PL/Venta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Venta
+    {
+        public void Add()
+        {
+            //Leer el Id de Usuario
+            ML.Venta venta = new ML.Venta();
+            venta.Usuario = new ML.Usuario();//instancia
+            venta.Usuario.IdUsuario = int.Parse(Console.ReadLine());
+
+            ML.Result result = new ML.Result();
+            result.Objects = new List<object>();
+            //while
+
+
+
+            //Venta.Add
+        }
+       
+       
+    }
+}
